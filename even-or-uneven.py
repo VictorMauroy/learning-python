5043549b67c1867a194cbd047a8726e08df4213c
VictorMauroy <mauroy.victor@gmail.com>
2023-05-30 11:23:52 +0200

    Feat: :sparkles: Add an even or uneven tester

---
+++ even-or-uneven.py
@@ -0,0 +1,6 @@
+nbToReceive = int(input("Entrez un nombre pair ou impair : "))
+
+if nbToReceive%2 == 0 :
+    print("Votre nombre est PAIR ! Félicitations !")
+else:
+    print("Votre nombre est IMPAIR ! Génial !")
