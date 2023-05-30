f87751bb75041ca631be555b24787120dacdf83e
VictorMauroy <mauroy.victor@gmail.com>
2023-05-30 16:25:10 +0200

    Feat: :sparkles: add examples for comprehension lists uses

---
+++ comprehension-list-sort.py
@@ -0,0 +1,11 @@
+import random
+#Do not forger to import "random"
+#Useful links : 
+# - https://python.doctor/page-comprehension-list-listes-python-cours-debutants
+# - https://gayerie.dev/docs/python/python3/list_comprehension.html
+
+randomListOfNumbers = [random.randint(1,100) for _ in range(1000)]
+#print(randomListOfNumbers)
+
+smallestNumbersList = [x for x in randomListOfNumbers if x < 10]
+print(smallestNumbersList)
