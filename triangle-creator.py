0056c45689917049ecd7f410728af473730f0e45
VictorMauroy <mauroy.victor@gmail.com>
2023-05-30 14:21:30 +0200

    Feat: :sparkles: Add a # triangle creator function

---
+++ triangle-creator.py
@@ -0,0 +1,18 @@
+def Make_Triangle(triangleSize:int) -> None :
+    """Make a triangle of '#' with a size specified by triangleSize
+
+    Args:
+        triangleSize (int): Heigh of the triangle
+    """
+    if triangleSize == 0 : return
+    for i in range(0, triangleSize) : 
+        message = "#"
+        for j in range(i):
+            message += "#"
+        print(message)
+
+Make_Triangle(
+    int(
+        input("Entrez la taille du triangle désiré : ")
+        )
+    )
