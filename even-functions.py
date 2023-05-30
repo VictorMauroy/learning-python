81ff21edb39e457ce4aa42a703ef85e6d7313930
VictorMauroy <mauroy.victor@gmail.com>
2023-05-30 13:07:21 +0200

    Feat: :sparkles: Add an advanced function for even or uneven

--- even-functions.py
+++ even-functions.py
@@ -1,7 +1,7 @@
 """ 
     def : mot clé pour déclarer une fonction
     nbToCheck:int ==> rajouter ":int" permet de déclarer le type de la valeur à recevoir
-    -> None ==> Permet de déterminer le type de valeur à retourner. Ici None car pas de "return"    
+    "-> None" ==> Permet d'indiquer le type de valeur à retourner. Ici None car pas de "return"    
 """
 def IsMyNumberEven(nbToCheck:int) -> None :
     """Determine if a number is even and return a boolean
@@ -14,8 +14,18 @@ def IsMyNumberEven(nbToCheck:int) -> None :
     else:
         print("le nombre est impair")
 
-    #return True if nbToCheck % 2 == 0 else False
-
+"""
 IsMyNumberEven(
     int(input("Entrez un nombre entier : "))
     )
+"""
+
+def est_pair(nb:int) -> bool :
+    """Determine if a number is even and return a boolean"""
+    return True if nb%2 == 0 else False
+
+numberOfOurUser = int(input("Veuillez entrer un nombre entier : "))
+
+print(
+    "Le nombre est pair" if est_pair(numberOfOurUser) else "Le nombre est impair"
+)
